e64dba3eff85cc7d0b7d57df6e1f08109b24159b
Mariana <mhuh0176@gmail.com>
2019-03-22 18:56:51 -0700

    added components

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Character extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+}
+
+export default Character;
---
+++ starwars/src/components/CharactersList.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Character from "./Character";
+
+class CharactersList extends Component {
+  render() {
+    return (
+      <div className="">
+
+      </div>
+    );
+  }
+}
+
+export default CharactersList;
